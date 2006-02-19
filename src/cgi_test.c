351f654a6928a0c465e35391763256e41eecdfe9
conrad <conrad@9c49b5d1-7df3-0310-bce2-b7278e68f44c>
2006-02-19 11:16:59 +0000

    add a test to run the cgi_main()
    git-svn-id: http://svn.kfish.org/fastphoto/trunk@156 9c49b5d1-7df3-0310-bce2-b7278e68f44c

---
+++ src/cgi_test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fastphoto.h"
+#include "cgi.h"
+#include "httpdate.h"
+
+int
+main (int argc, char * argv[])
+{
+  fastphoto_t params;
+  int err = 0;
+
+  memset (&params, 0, sizeof (fastphoto_t));
+
+  httpdate_init ();
+
+  setenv ("PATH_INFO", "/TEST/test.jpg", 1);
+  setenv ("PATH_TRANSLATED", "./test.jpg", 1);
+  setenv ("QUERY_STRING", "width=100", 1);
+  
+  err = cgi_main (&params);
+
+  if (params.data) free (params.data);
+
+  if (err) return 1;
+  else return 0;
+}
