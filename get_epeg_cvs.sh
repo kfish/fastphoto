dce152c43107414c5c8eee4518f997674d04c5e2
conrad <conrad@9c49b5d1-7df3-0310-bce2-b7278e68f44c>
2008-02-23 04:43:12 +0000

    update epeg cvs location
    git-svn-id: http://svn.kfish.org/fastphoto/trunk@214 9c49b5d1-7df3-0310-bce2-b7278e68f44c

--- get_epeg_cvs.sh
+++ get_epeg_cvs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/enlightenment co e17/libs/epeg
+cvs -z3 -d :pserver:anonymous@anoncvs.enlightenment.org:/var/cvs/e co e17/libs/epeg
 mv e17/libs/epeg . && rm -rf e17
 cd epeg && ./autogen.sh && make
